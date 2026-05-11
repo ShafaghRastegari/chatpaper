df519a67bc2ecbb4f96b694ab3d7a20653c09e4f
ShafaghRastegari <shafaghrastegari@gmail.com>
2026-05-11 10:56:48 +0200

    remove unnecesserily import in pipeline file

--- src/rag/pipeline.py
+++ src/rag/pipeline.py
@@ -6,7 +6,6 @@ from llama_index.core import VectorStoreIndex, Document, Settings, StorageContex
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.vector_stores.chroma import ChromaVectorStore
 from llama_index.llms.openai_like import OpenAILike
-from llama_index.embeddings.fastembed import FastEmbedEmbedding
 from llama_index.embeddings.huggingface_api import HuggingFaceInferenceAPIEmbedding
 
 
